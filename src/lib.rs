a8ae37071097bed730160691d0699836a7d831ec
Andrea Pelloni <apelloni90@gmail.com>
2025-02-10 11:24:02 +0100

    support cloning (not yet cloning the ps points

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod uux_aag;
 pub trait MG5Integrand
 where
     Self: Default,
+    Self: Clone,
 {
     fn init(card_path: &str) -> Self;
     fn set_card(&mut self, card_path: &str);
@@ -18,4 +19,18 @@ where
     fn n_processes(&self) -> usize;
     fn name(&self) -> String;
     fn cout(&mut self) -> String;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn clone() {
+        let integrand = uux_aag::RustMG5::init("./mg5/cards/param_card.dat");
+        let mut vec: Vec<uux_aag::RustMG5> = vec![];
+        for _ in 0..1000 {
+            vec.push(integrand.clone());
+        }
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -6,7 +6,26 @@ macro_rules! rmg5 {
         /// Wrapper for rust of the CPP bridge and the function calls
         pub struct RustMG5 {
             integrand: cxx::UniquePtr<$mg5_bridge>,
+            param_card: Option<String>,
             initialized: bool,
+        }
+
+        impl Clone for RustMG5 {
+            fn clone(&self) -> Self {
+                let mut new_ptr = ffi::new_mg5_integrand();
+                if self.initialized {
+                    unsafe {
+                        let card_path = self.param_card.clone().unwrap();
+                        let c_path = std::ffi::CString::new(card_path.as_str()).unwrap();
+                        new_ptr.as_mut().unwrap().init(c_path.as_ptr());
+                    }
+                }
+                Self {
+                    integrand: new_ptr,
+                    param_card: self.param_card.clone(),
+                    initialized: self.initialized.clone(),
+                }
+            }
         }
 
         impl Default for RustMG5 {
@@ -16,10 +35,12 @@ macro_rules! rmg5 {
                 assert!(mg5_integrand.nprocesses() == 1);
                 Self {
                     integrand: mg5_integrand,
+                    param_card: None,
                     initialized: false,
                 }
             }
         }
+
         impl crate::MG5Integrand for RustMG5 {
             /// Initialise integrand with the parameter card
             fn init(card_path: &str) -> Self {
@@ -38,6 +59,7 @@ macro_rules! rmg5 {
                 }
                 Self {
                     integrand: mg5_integrand,
+                    param_card: Some(String::from(card_path)),
                     initialized: true,
                 }
             }
@@ -54,11 +76,19 @@ macro_rules! rmg5 {
                     let c_path = std::ffi::CString::new(card_path).unwrap();
                     self.integrand.as_mut().unwrap().init(c_path.as_ptr());
                 }
+                self.param_card = Some(String::from(card_path));
                 self.initialized = true;
             }
 
             /// Set momenta for the evaluation
             fn set_externals(&mut self, flatten_moms: &[f64]) {
+                assert_eq!(
+                    self.n_externals() * 4,
+                    flatten_moms.len(),
+                    "Expected vector of length {} but found {}",
+                    self.n_externals() * 4,
+                    flatten_moms.len()
+                );
                 // Flatten data
                 unsafe {
                     self.integrand
